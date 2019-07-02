7169110857fc0e6ee356b17a27e587123109fa19
ashahtango <ashah@tangocode.com>
2019-07-02 14:27:56 -0500

    laying down framework and adding in AddSubviews and Shake convenience methods

--- Example/KibbleLib/ViewController.swift
+++ Example/KibbleLib/ViewController.swift
@@ -2,23 +2,55 @@
 //  ViewController.swift
 //  KibbleLib
 //
-//  Created by aviatoali on 07/02/2019.
-//  Copyright (c) 2019 aviatoali. All rights reserved.
+//  Created by Ali H. Shah on 07/02/2019.
+//  Copyright (c) 2019 Ali H. Shah. All rights reserved.
 //
 
+import KibbleLib
 import UIKit
 
 class ViewController: UIViewController {
+    weak var testView: UIView!
+    weak var testView2: UIView!
+    
+    override func loadView() {
+        super.loadView()
+        let testView = UIView()
+        testView.translatesAutoresizingMaskIntoConstraints = false
+        let testView2 = UIView()
+        testView2.translatesAutoresizingMaskIntoConstraints = false
+        view.AddSubviews(testView, testView2)
+        NSLayoutConstraint.activate([
+            testView.heightAnchor.constraint(equalToConstant: 64),
+            testView.widthAnchor.constraint(equalTo: testView.heightAnchor),
+            testView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+        self.testView = testView
+        NSLayoutConstraint.activate([
+            testView2.heightAnchor.constraint(equalTo: testView.heightAnchor),
+            testView2.widthAnchor.constraint(equalTo: testView.widthAnchor),
+            testView2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testView2.bottomAnchor.constraint(equalTo: testView.topAnchor)
+            ])
+        let tapReco = UITapGestureRecognizer(target: self, action: #selector(self.shakeView (_:)))
+        testView2.addGestureRecognizer(tapReco)
+        self.testView2 = testView2
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
+        testView.backgroundColor = .yellow
+        testView2.backgroundColor = .green
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func shakeView(_ sender: UITapGestureRecognizer) {
+        self.testView2.Shake()
+    }
 }
 
---
+++ KibbleLib/Classes/UIViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  KibbleLib
+//
+//  Created by Shan-e-Ali Shah on 7/2/19.
+//
+
+import Foundation
